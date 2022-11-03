ff001f1d655c13917cd3db9e14cacb963472d792
DaviBraz7 <davisouzabraz@gmail.com>
2022-11-03 15:49:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+            
+            Console.WriteLine(Math.Floor(valor));
+
+        
+            Console.WriteLine(Math.Ceiling(valor));
+
+            
+            Console.WriteLine(Math.Round(valor));
+
+            /////////////////////////
+            /*decimal valor = 10.25m;
+            
+            Console.WriteLine(valor.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));*/
+        }
+    }
+}
